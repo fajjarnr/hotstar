3e3a87d69baf5d78ad921ae34a4929ee262b6e65
jay <jay@jay.localdomain>
2021-04-28 19:54:58 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
